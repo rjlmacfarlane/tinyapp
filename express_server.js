5e5d531f488c4b7da707ec567c39f9b89aca6253
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-07 15:50:49 -0400

    Add /urls/:shortURL route

--- express_server.js
+++ express_server.js
@@ -8,6 +8,7 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+// basic server I/O...
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -20,11 +21,19 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+// res.renders..
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  res.render('urls_show', templateVars);
+});
+
+// example code..
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
