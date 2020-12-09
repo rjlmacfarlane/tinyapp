55448988784e8a5e067568a39218d3f6acaded31
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-09 15:14:59 -0400

    Completed user registration and login/logout functionality

--- express_server.js
+++ express_server.js
@@ -8,27 +8,44 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+// Helper functions..
+
+// Generate a faux-random string (for URL shortening):
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(2,8);
+};
+
+// Generate a faux-random UID:
+const generateUid = function() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+};
+
+const users = new Object;
+
+// Lookup user by email:
+const getUserEmail = function(email, users) {
+  for (let i in users) {
+    if (users[i].email === email) {
+      return Object.values(users);
+    }
+  } return false;
+};
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-const users = new Object;
-
 // basic server I/O...
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  const userID = req.cookies['user_id'];
+  const templateVars = {
+    user: users[userID]
+  };
+  res.send('urls_index', templateVars);
 });
 
-app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}...`);
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-// res.renders..
+// Get/Renders..
 app.get('/urls', (req, res) => {
   const userID = req.cookies['user_id'];
   const templateVars = {
@@ -40,16 +57,22 @@ app.get('/urls', (req, res) => {
 
 // Create a new shortened URL..
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const userID = req.cookies['user_id'];
+  const templateVars = {
+    urls: urlDatabase, // Probably don't need this - check!
+    user: users[userID]
+  };
+  res.render('urls_new', templateVars);
 });
 
 // Retrive long URL by short URL..
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
+  const userID = req.cookies['user_id'];
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL],
-    username: req.cookies["username"],
+    user: userID
   };
   res.render('urls_show', templateVars);
 });
@@ -60,35 +83,78 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 });
 
-// Register
+// Register (GET => render)
 app.get('/register', (req, res) => {
+  const userID = req.cookies['user_id'];
   const templateVars = {
-    username: req.cookies["username"]
+    user: userID
   };
   res.render('register', templateVars);
 });
 
-app.post('/register', (req, res) => {
-  const uid = generateUid();
-  const newUser = {
-    id: uid,
-    email: req.body.email,
-    password: req.body.password
+// Login (GET => render)
+app.get('/login', (req, res) => {
+  const userID = req.cookies['user_id'];
+  const templateVars = {
+    user: userID
   };
-  users[uid] = newUser;
-  res.cookie('user_id', uid);
-  res.redirect('/urls');
+  res.render('login', templateVars);
+});
+
+// Register (POST => redirect)   Declare templateVars at top, remove lets
+app.post('/register', (req, res) => {
+  res.clearCookie('user_id');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    let templateVars = {
+      status: 401,
+      message: 'Email or Password must not be blank.',
+      user: 'undefined'
+    };
+    res.status(401);
+    return res.render('reg_error', templateVars);
+  }
+  const user = getUserEmail(email, users);
+  if (user) {
+    let templateVars = {
+      status: 409,
+      message: 'Email already registered.',
+      user: 'undefined'
+    };
+    res.status(409);
+    res.render('reg_error', templateVars);
+  } else {
+    const uid = generateUid();
+    const newUser = {
+      id: uid,
+      email: req.body.email,
+      password: req.body.password
+    };
+    users[uid] = newUser;
+    res.cookie('user_id', uid);
+    res.redirect('/urls');
+  }
+  // console.log(users);
 });
 
 // Login
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
+  res.clearCookie('user_id');
+  const { email, password } = req.body;
+  const user = getUserEmail(email, users);
+  if (!user) {
+    res.status(403).send('Email not found');
+  } else if (user[0].password !== password) {
+    res.status(403).send('Password incorrect');
+  } else {
+    res.cookie('user_id', user[0].id);
+    res.redirect('/urls');
+  }
 });
 
 // Logout
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
@@ -101,7 +167,7 @@ app.post('/urls', (req, res) => {
 });
 
 // Edit an existing URL..
-app.post('/urls/:shortURL/edit', (req, res) => {
+app.post('/urls/:shortURL/edit', (req, res) => {   // app.post should be without /edit
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
@@ -116,16 +182,9 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
-
-const generateRandomString = function() {
-  return Math.random().toString(36).substring(2,8);
-};
-
-const generateUid = function() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-};
-
-
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}...`);
+});
 
 // // example code..
 // app.get('/hello', (req, res) => {
